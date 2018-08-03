39c0eb5dfd6de7aac6967078b13f86c611726fbf
Ruslan Drugak <russik.drug@yandex.ru>
2018-08-04 01:11:36 +0300

    add divece setting system

---
+++ database/migrations/2018_08_03_214341_create_devise_settings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeviseSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devise_settings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->morphs('devices');
+            $table->morphs('settings');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devise_settings');
+    }
+}
