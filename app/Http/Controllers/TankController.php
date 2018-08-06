ab381ae07d805b945cbaf969c96ccf2f971d3e10
Vladislav <sklyarov.vshm@gmail.com>
2018-08-06 10:05:23 +0300

    added and routed resource controller 'TankController'

---
+++ app/Http/Controllers/TankController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tank;
+use Illuminate\Http\Request;
+
+class TankController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tank  $tank
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tank $tank)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Tank  $tank
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tank $tank)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tank  $tank
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tank $tank)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tank  $tank
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tank $tank)
+    {
+        //
+    }
+}
