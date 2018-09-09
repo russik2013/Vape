3326f0882eadd9c78be706b1662807002b7698fd
Vladislav <sklyarov.vshm@gmail.com>
2018-09-09 19:18:06 +0300

    model SettingType completed

--- app/Models/SettingType.php
+++ app/Models/SettingType.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class SettingType extends Model
 {
-    //
+    protected $table = 'setting_types';
+
+    protected $fillable = ['name'];
+
+    public function settings(){
+        return $this->hasMany( Setting::class );
+    }
 }
